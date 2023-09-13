1e7398b5fdb5d2b9108158b6b7644fd68552dee5
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2023-09-13 11:21:54 -0300

    arrumando problemas

--- Program.cs
+++ Program.cs
@@ -3,17 +3,17 @@ string variavel1, variavel2, auxiliar;
 
 Console.WriteLine("--- Troca de Valores ---\n");
 
-Console.Write("variave 1..: ");
-Variavel1 = Console.ReadLine()!;
+Console.Write("variavel 1..: ");
+variavel1 = Console.ReadLine()!;
 
 Console.Write("Variavel 2..: ");
 variavel2 = Console.ReadLine()!;
 
-auxiliar = variave1;
-variave1 = variave2;
-variave2 = auxiliar;
+auxiliar = variavel1;
+variavel1 = variavel2;
+variavel2 = auxiliar;
 
 Console.WriteLine("\nApós trocar os valores temos:\n");
 
-Console.WriteLine($"variave 1 = {variave1}");
-Console.WriteLine($"variave 2 = {variave2}");
+Console.WriteLine($"variavel 1 = {variavel1}");
+Console.WriteLine($"variavel 2 = {variavel2}");
